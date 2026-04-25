e5eb2c125920e117372b390a4629b75f8977bddb
jlnshly <jelaineashleyramos@gmail.com>
2026-04-25 14:27:22 +0800

    importing next function list and tuple

--- number_processor.py
+++ number_processor.py
@@ -1,4 +1,6 @@
 import logging
+from typing import List, Tuple
+
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     datefmt='%H:%M:%S')
