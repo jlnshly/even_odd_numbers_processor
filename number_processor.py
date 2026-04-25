021cedff252208dd8c24666530b1e9735213563c
jlnshly <jelaineashleyramos@gmail.com>
2026-04-25 14:25:08 +0800

    logging functions done

--- number_processor.py
+++ number_processor.py
@@ -1,2 +1,6 @@
 import logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    datefmt='%H:%M:%S')
+
+
