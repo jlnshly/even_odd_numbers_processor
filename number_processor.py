94f051179d649d238ea8873673733aed1b18dbf3
jlnshly <jelaineashleyramos@gmail.com>
2026-04-25 14:32:37 +0800

    Coding base class for file handling

--- number_processor.py
+++ number_processor.py
@@ -1,8 +1,15 @@
 import logging
 from typing import List, Tuple
+from pathlib import Path
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     datefmt='%H:%M:%S')
 
+class NumberProcessorBase:
+    #Base class that handles file operations
+    def __init__(self, input_path: str):
+        self.input_path = Path(input_path)
 
+    def file_validation (self) -> bool:
+        if not self.input_path.exists():
